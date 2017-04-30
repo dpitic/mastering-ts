0d41b917541e458587db0313d0ea9968e86104d0
Dan Pitic <dpitic@gmail.com>
2017-04-30 14:48:45 +0800

    Completed generics. Up next: Asynchronous language features.

--- ch04/hello_ch04.js
+++ ch04/hello_ch04.js
@@ -1,6 +1,16 @@
 // Decorators
 // ==========
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -278,4 +288,85 @@ var myArrayResult = myArrayConcatenator.concatenateArray(myArray);
 console.log(myArrayResult);
 // Constraining the type of T
 // ==========================
+var ClubHomeCountry;
+(function (ClubHomeCountry) {
+    ClubHomeCountry[ClubHomeCountry["England"] = 0] = "England";
+    ClubHomeCountry[ClubHomeCountry["Germany"] = 1] = "Germany";
+})(ClubHomeCountry || (ClubHomeCountry = {}));
+var FootballClub = (function () {
+    function FootballClub() {
+    }
+    FootballClub.prototype.getName = function () { return this._name; };
+    ;
+    FootballClub.prototype.getHomeCountry = function () { return this._homeCountry; };
+    ;
+    return FootballClub;
+}());
+var Liverpool = (function (_super) {
+    __extends(Liverpool, _super);
+    function Liverpool() {
+        var _this = _super.call(this) || this;
+        _this._name = "Liverpool F.C.";
+        _this._homeCountry = ClubHomeCountry.England;
+        return _this;
+    }
+    return Liverpool;
+}(FootballClub));
+var BorussiaDortmund = (function (_super) {
+    __extends(BorussiaDortmund, _super);
+    function BorussiaDortmund() {
+        var _this = _super.call(this) || this;
+        _this._name = "Borussia Dortmund";
+        _this._homeCountry = ClubHomeCountry.Germany;
+        return _this;
+    }
+    return BorussiaDortmund;
+}(FootballClub));
+// IFootballClubPrinter<T> defined below in Generic Interfaces
+var FootballClubPrinter = (function () {
+    function FootballClubPrinter() {
+    }
+    FootballClubPrinter.prototype.print = function (arg) {
+        console.log(" " + arg.getName() + " is" +
+            (" " + this.IsEnglishTeam(arg) + " ") +
+            "an English football team.");
+    };
+    ;
+    FootballClubPrinter.prototype.IsEnglishTeam = function (arg) {
+        if (arg.getHomeCountry() == ClubHomeCountry.England) {
+            return "";
+        }
+        else {
+            return "NOT";
+        }
+    };
+    return FootballClubPrinter;
+}());
+var clubInfo = new FootballClubPrinter();
+clubInfo.print(new Liverpool());
+clubInfo.print(new BorussiaDortmund());
+// Creating new objects within generics
+// ====================================
+var FirstClass = (function () {
+    function FirstClass() {
+    }
+    return FirstClass;
+}());
+var SecondClass = (function () {
+    function SecondClass() {
+    }
+    return SecondClass;
+}());
+var GenericCreator = (function () {
+    function GenericCreator() {
+    }
+    GenericCreator.prototype.create = function (arg1) {
+        return new arg1();
+    };
+    return GenericCreator;
+}());
+var creator1 = new GenericCreator();
+var firstClass = creator1.create(FirstClass);
+var creator2 = new GenericCreator();
+var secondClass = creator2.create(SecondClass);
 //# sourceMappingURL=hello_ch04.js.map
--- ch04/hello_ch04.ts
+++ ch04/hello_ch04.ts
@@ -218,24 +218,24 @@ class ClassWithMetaData {
 // ========
 
 class Concatenator<T> {
-	concatenateArray(inputArray: Array<T>): string {
-		let returnString = "";
+  concatenateArray(inputArray: Array<T>): string {
+    let returnString = "";
 
-		for (let i = 0; i < inputArray.length; i++) {
-			if (i > 0) {
-				returnString += ",";
-			}
-			returnString += inputArray[i].toString();
-		}
-		return returnString;
-	}
+    for (let i = 0; i < inputArray.length; i++) {
+      if (i > 0) {
+        returnString += ",";
+      }
+      returnString += inputArray[i].toString();
+    }
+    return returnString;
+  }
 }
 
 var stringConcat = new Concatenator<string>();
 var numberConcat = new Concatenator<number>();
 
 let concatResult = stringConcat.concatenateArray(
-	["first", "second", "third"]);
+  ["first", "second", "third"]);
 console.log(concatResult);
 
 var stringArray: string[] = ["first", "second", "third"];
@@ -249,19 +249,19 @@ console.log(numberResult);
 // ================
 
 class MyClass {
-	private _name: string;
-	constructor(arg1: number) {
-		this._name = arg1 + "_MyClass";
-	}
-	toString(): string {
-		return this._name;
-	}
+  private _name: string;
+  constructor(arg1: number) {
+    this._name = arg1 + "_MyClass";
+  }
+  toString(): string {
+    return this._name;
+  }
 }
 
 let myArray: MyClass[] = [
-	new MyClass(1),
-	new MyClass(2),
-	new MyClass(3)
+  new MyClass(1),
+  new MyClass(2),
+  new MyClass(3)
 ]
 
 let myArrayConcatenator = new Concatenator<MyClass>();
@@ -271,3 +271,87 @@ console.log(myArrayResult);
 // Constraining the type of T
 // ==========================
 
+enum ClubHomeCountry {
+  England,
+  Germany
+}
+
+interface IFootballClub {
+  getName(): string;
+  getHomeCountry(): ClubHomeCountry;
+}
+
+abstract class FootballClub implements IFootballClub {
+  protected _name: string;
+		protected _homeCountry: ClubHomeCountry;
+		getName() { return this._name };
+		getHomeCountry() { return this._homeCountry };
+}
+
+class Liverpool extends FootballClub {
+  constructor() {
+    super();
+    this._name = "Liverpool F.C.";
+    this._homeCountry = ClubHomeCountry.England;
+  }
+}
+
+class BorussiaDortmund extends FootballClub {
+  constructor() {
+    super();
+    this._name = "Borussia Dortmund";
+    this._homeCountry = ClubHomeCountry.Germany;
+  }
+}
+
+// IFootballClubPrinter<T> defined below in Generic Interfaces
+
+class FootballClubPrinter<T extends IFootballClub>
+  implements IFootballClubPrinter<T> {
+  print(arg: T) {
+    console.log(` ${arg.getName()} is` +
+      ` ${this.IsEnglishTeam(arg)} ` +
+      `an English football team.`);
+  };
+	IsEnglishTeam(arg: T): string {
+		if (arg.getHomeCountry() == ClubHomeCountry.England) {
+			return "";
+		} else {
+			return "NOT";
+		}
+	}
+}
+
+let clubInfo = new FootballClubPrinter();
+clubInfo.print(new Liverpool());
+clubInfo.print(new BorussiaDortmund());
+
+// Generic Interfaces
+// ==================
+
+interface IFootballClubPrinter<T extends IFootballClub> {
+	print(arg: T);
+	IsEnglishTeam(arg: T);
+}
+
+// Creating new objects within generics
+// ====================================
+
+class FirstClass {
+	id: number;
+}
+
+class SecondClass {
+	name: string;
+}
+
+class GenericCreator<T> {
+	create(arg1: { new(): T}): T {
+		return new arg1();
+	}
+}
+ var creator1 = new GenericCreator<FirstClass>();
+ var firstClass: FirstClass = creator1.create(FirstClass);
+
+ var creator2 = new GenericCreator<SecondClass>();
+ var secondClass: SecondClass = creator2.create(SecondClass);
