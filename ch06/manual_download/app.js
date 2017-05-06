4c9e6f3e050117bc50b54175dd82af2d45581984
Dan Pitic <dpitic@gmail.com>
2017-05-06 16:41:22 +0800

    Completed chapter 6.

---
+++ ch06/manual_download/app.js
@@ -0,0 +1,5 @@
+console.log("hello app.js");
+$(document).ready(function () {
+    $("#content").html("<h3>Hello TypeScript");
+});
+//# sourceMappingURL=app.js.map
---
+++ ch06/manual_download/app.ts
@@ -0,0 +1,5 @@
+console.log(`hello app.js`);
+
+$(document).ready( () => {
+  $("#content").html(`<h3>Hello TypeScript`);
+});
